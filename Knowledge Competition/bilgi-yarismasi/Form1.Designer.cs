ca8738c6a4709703845906916339f73a4ce4cf98
Azat <azat.bal@hotmail.com>
2023-07-18 19:17:56 +0300

    Knowledge Competition

---
+++ Knowledge Competition/bilgi-yarismasi/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace bilgi_yarismasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.Location = new System.Drawing.Point(67, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(371, 127);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(84, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(287, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 59);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(84, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 59);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button4.Location = new System.Drawing.Point(287, 272);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 59);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(500, 70);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 59);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "SIRADAKİ SORU";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(626, 352);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 48);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1.) 500 TL";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(626, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 48);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2.) 1.000 TL";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(626, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 48);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "3.) 5.000 TL";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(626, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 48);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "4.) 10.000 TL";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(626, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 48);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "5.) 100.000 TL";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(690, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ Knowledge Competition/bilgi-yarismasi/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bilgi_yarismasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button5.Text = "BAŞLA";
+        }
+        int dogrusayisi=0, yanlissayisi=0;
+        int soru_numarasi = 0;
+        string dogru_cevap;
+        string kullanici_cevabi;
+        bool sorubir, soruiki, soruüç, sorudört, sorubeş;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+            kullanici_cevabi = button1.Text;
+            if (kullanici_cevabi == dogru_cevap)
+            {
+                dogrusayisi++;
+               
+
+            }
+            else
+            {
+
+                soru_numarasi = 0;
+                MessageBox.Show("Yanlış Cevap!!!\nKaybettiniz!!! :( ");
+                label1.BackColor = Color.Green;
+                label2.BackColor=Color.Green;
+                label3.BackColor = Color.Green;
+                label4.BackColor = Color.Green;
+                label5.BackColor = Color.Green;
+
+            }
+            
+            label6.Text = dogrusayisi.ToString();
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+            kullanici_cevabi = button2.Text;
+            if (kullanici_cevabi == dogru_cevap)
+            {
+                dogrusayisi++;
+                
+            }
+            else
+            {
+
+                soru_numarasi = 0;
+                MessageBox.Show("Yanlış Cevap!!!\nKaybettiniz!!! :( ");
+                label1.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label4.BackColor = Color.Green;
+                label5.BackColor = Color.Green;
+
+            }
+
+            label6.Text = dogrusayisi.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+            kullanici_cevabi = button3.Text;
+            if (kullanici_cevabi == dogru_cevap)
+            {
+                dogrusayisi++;
+                
+            }
+            else
+            {
+                soru_numarasi = 0;
+                MessageBox.Show("Yanlış Cevap!!!\nKaybettiniz!!! :( ");
+                label1.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label4.BackColor = Color.Green;
+                label5.BackColor = Color.Green;
+            }
+            label6.Text = dogrusayisi.ToString();
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+            kullanici_cevabi = button4.Text;
+            if (kullanici_cevabi == dogru_cevap)
+                dogrusayisi++;
+            else
+            {
+
+                soru_numarasi = 0;
+                MessageBox.Show("Yanlış Cevap!!!\nKaybettiniz!!! :( ");
+                label1.BackColor = Color.Green;
+                label2.BackColor = Color.Green;
+                label3.BackColor = Color.Green;
+                label4.BackColor = Color.Green;
+                label5.BackColor = Color.Green;
+
+            }
+            label6.Text = dogrusayisi.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = false;
+            soru_numarasi++;
+            button5.Text = "SIRADAKİ SORU";
+            if (soru_numarasi == 1)
+            {
+                richTextBox1.Text = "Türkiye'nin başkenti neresidir?";
+                button1.Text = "Ankara";
+                button2.Text = "Konya";
+                button3.Text = "İstanbul";
+                button4.Text = "Sivas";
+                dogru_cevap = "Ankara";
+
+            }
+            if(soru_numarasi == 2)
+            {
+                richTextBox1.Text = "İstanbul kaç yılında fethedilmiştir?";
+                button1.Text = "1441";
+                button2.Text = "1453";
+                button3.Text = "1461";
+                button4.Text = "1465";
+                dogru_cevap = "1453";
+                label1.BackColor = Color.Blue;
+            }
+            if (soru_numarasi == 3)
+            {
+                richTextBox1.Text = "Hangisinin yazılışı YANLIŞTIR?";
+                button1.Text = "Hamburger";
+                button2.Text = "Trileçe";
+                button3.Text = "Antrikot";
+                button4.Text = "Çipsi";
+                dogru_cevap = "Çipsi";
+                label2.BackColor = Color.Blue;
+
+            }
+            if (soru_numarasi == 4)
+            {
+                richTextBox1.Text = "Hangi oyuncu Fenerbahçe'de hiç OYNAMAMIŞTIR?";
+                button1.Text = "Okan Buruk";
+                button2.Text = "Burak Yılmaz";
+                button3.Text = "Mehmet Topal";
+                button4.Text = "Serdar Aziz";
+                dogru_cevap = "Okan Buruk";
+                label3.BackColor = Color.Blue;
+            }
+            if (soru_numarasi == 5)
+            {
+                richTextBox1.Text = "*******FİNAL SORU*****\nEşeknar üçgenin bir dış açısı kaç derecedir?";
+                button1.Text = "30";
+                button2.Text = "60";
+                button3.Text = "120";
+                button4.Text = "135";
+                dogru_cevap="120";
+                label4.BackColor = Color.Blue;
+            }
+            if (soru_numarasi == 6)
+            {
+                MessageBox.Show("********** TEBRİKLER *********\n**********1.000.000 TL KAZANDINIZ *********");
+                button5.Enabled = false;
+            }
+
+
+        }
+    }
+}
